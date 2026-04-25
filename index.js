54c4585b839a0c9ac4cc81bde35b0ab0c0e30e79
Terminator473 <g.o.rowe@icloud.com>
2026-04-25 17:54:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Happy developing ✨')
+console.log('Happy developing ✨')
+function extend() {
+    let header = document.getElementById("header");
+    
+    if (header.style.width === "30%") {
+        header.style.width = "80%";
+    } else {
+        header.style.width = "30%";
+    }
+}
